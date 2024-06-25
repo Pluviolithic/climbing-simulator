b85752a6803f42987250083a626f5cbf2646c864
Eric McKanna <Eric.McKanna@is4s.com>
2024-06-25 14:16:48 -0400

    Add commands and change stat defaults

---
+++ src/server/Commands/Players/AwardAllGamepasses.lua
@@ -0,0 +1,13 @@
+return {
+	Name = "awardallgamepasses",
+	Aliases = { "aag" },
+	Description = "Gives a player all the gamepasses.",
+	Group = "DefaultAdmin",
+	Args = {
+		{
+			Type = "player",
+			Name = "Player",
+			Description = "The player who you want to award the product to.",
+		},
+	},
+}

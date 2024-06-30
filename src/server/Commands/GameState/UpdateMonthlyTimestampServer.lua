fa17b97a0a165c5c6d5091b56600db468a0db409
Eric McKanna <Eric.McKanna@is4s.com>
2024-06-30 19:14:46 -0400

    Switch from Rodux to Reflex

--- src/server/Commands/GameState/UpdateMonthlyTimestampServer.lua
+++
@@ -1,7 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local clockUtils = require(ReplicatedStorage.Common.Utils.ClockUtils)
-
-return function(_, timestamp: string)
-	return clockUtils.setMonthlyTimestampOverride(timestamp)
-end
